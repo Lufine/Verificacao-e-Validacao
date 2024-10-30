875adfa0ec5e47fcfeacdcf63280acaa431f81ef
Lufine <lfneuwirth@gmail.com>
2024-10-30 15:29:45 -0300

    Fase Red: Escrever testes para calcular a média

--- media_calculadora.py
+++ media_calculadora.py
@@ -1,3 +1,2 @@
 def calcular_media(nota1, nota2, nota3):
-    # Arredonda o resultado para duas casas decimais
-    return round((nota1 + nota2 + nota3) / 3, 2)
+    pass  # A implementação será feita na Fase Green
